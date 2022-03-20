f883a6ebe5e9f2f22472ce1014a4b5d1694490c7
Roni Polak <polak.roni@gmail.com>
2022-03-20 13:01:48 +0200

    fixed file paths

--- Face-filter/StreamLit.py
+++ Face-filter/StreamLit.py
@@ -44,6 +44,7 @@ def show_final_img(filters, file_uploaded, is_filters):
     if (file_uploaded is not None):#uploaded file
         data, resized_img = get_image_data(file_uploaded)  # gets image as array data and resized image
         final_image = Source.run(data, resized_img, filters, is_filters)
+        print(final_image)
         if(len(final_image) > 0):#If face was found
             st.image(final_image)#shows image
         else:
